25249ac783919a3df31f7d6aa5ad2ba29ef2d7dc
Navodya <navodyadhanushka01@gmail.com>
2024-08-26 21:14:27 +0530

    Appoinment 3 pages added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import SelectServices from './Pages/Appoinment/SelectServices';
+import SelectProfessional from './Pages/Appoinment/SelectProfessional';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SelectServices />} />
+        <Route path="/professional" element={<SelectProfessional />} />
+      </Routes>
+    </Router>
   );
-}
+} 
 
 export default App;
